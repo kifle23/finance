d71a851dbeef9d9baedf3363f8eec78e0b9062f0
kifle23 <kiflekifle23@gmail.com>
2024-03-22 10:20:44 +0300

    Add cardlist component

---
+++ frontend/src/components/CardList/CardList.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import './CardList.css';
+
+interface CardListProps {}
+
+const CardList: FC<CardListProps> = () => (
+  <div className="CardList">
+    CardList Component
+  </div>
+);
+
+export default CardList;
