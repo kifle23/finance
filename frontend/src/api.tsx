85cb385e0eb6f5200d5b4e3df7f8c632da6e9c86
kifle23 <kiflekifle23@gmail.com>
2024-03-25 20:25:39 +0300

    Add financial api

---
+++ frontend/src/api.tsx
@@ -0,0 +1,21 @@
+import axios from "axios";
+import { CompanySearch } from "./company";
+
+interface SearchResponse {
+    data: CompanySearch[];
+}
+
+export const searchCompanies = async (query: string) => {
+  try {
+    const data = await axios.get<SearchResponse>(
+        `https://financialmodelingprep.com/api/v3/search-ticker?query=${query}&limit=10&exchange=NASDAQ&apikey=${process.env.REACT_APP_API_KEY}`
+        );
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.error(error.response?.data);
+    } else {
+      console.error(error);
+    }
+  }
+}; 
