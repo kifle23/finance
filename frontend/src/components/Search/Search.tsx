d3a7e684b27fbe16809c1ce7cbff548666c619d4
kifle23 <kiflekifle23@gmail.com>
2024-03-25 18:58:19 +0300

    Add button for search component

--- frontend/src/components/Search/Search.tsx
+++ frontend/src/components/Search/Search.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from 'react';
+import React, { ChangeEvent, FC, MouseEvent, useState } from 'react';
 import './Search.css';
 
 interface Props {}
@@ -6,18 +6,24 @@ interface Props {}
 const Search: FC<Props> = (props:Props) => {
   const [search, setSearch] = useState('');
   
-  const onclick = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
     console.log(e);
   };
+
+  const handleClick = (e:MouseEvent<HTMLButtonElement>) => {
+    console.log('Search:', search);
+  };
+
   return (
   <div className="Search">
     <input
       type="text"
       placeholder="Search..."
       value={search}
-      onChange={(e)=> onclick(e)}
+      onChange={(e)=> handleChange(e)}
     />
+    <button onClick={(e) => handleClick(e)}>Search</button>
   </div>
   );
 
