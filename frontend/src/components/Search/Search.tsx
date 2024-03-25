b7563f24d828e6fc56f8fce3e83b1c2a0a3543bb
kifle23 <kiflekifle23@gmail.com>
2024-03-25 10:58:22 +0300

    Generate search component

---
+++ frontend/src/components/Search/Search.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import './Search.css';
+
+interface SearchProps {}
+
+const Search: FC<SearchProps> = () => (
+  <div className="Search">
+    Search Component
+  </div>
+);
+
+export default Search;
