dca31721775952356526d1761801d4ed7ee464bc
kifle23 <kiflekifle23@gmail.com>
2024-04-17 09:19:31 +0300

    Reafctor search and correct data flow

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,23 @@
+import React, { ChangeEvent, MouseEvent, useState } from 'react';
 import './App.css';
 import CardList from './components/CardList/CardList';
 import Search from './components/Search/Search';
 
 function App() {
+  const [search, setSearch] = useState('');
+  
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+    console.log(e);
+  };
+
+  const handleClick = (e:MouseEvent<HTMLButtonElement>) => {
+    console.log('Search:', search);
+  };
+
   return (
     <div className="App">
-      <Search />
+      <Search handleClick = { handleClick } handleChange = {handleChange} search= { search } />
       <CardList />
     </div>
   );
