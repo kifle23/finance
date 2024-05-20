f8da75ba17cde95efce2a0a0b686a70f2f564eb3
kifle23 <kiflekifle23@gmail.com>
2024-05-20 16:08:36 +0300

    Create card portfolio

---
+++ frontend/src/components/Portfolio/CardPortfolio/CardPortfolio.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import './CardPortfolio.css';
+
+interface CardPortfolioProps {}
+
+const CardPortfolio: FC<CardPortfolioProps> = () => (
+  <div className="CardPortfolio">
+    CardPortfolio Component
+  </div>
+);
+
+export default CardPortfolio;
