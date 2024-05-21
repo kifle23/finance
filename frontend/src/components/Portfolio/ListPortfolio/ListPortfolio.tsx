2678fe8171712a3e2ef257fabcd0a5cf7c045ded
kifle23 <kiflekifle23@gmail.com>
2024-05-21 13:19:55 +0300

    Passdown onPortfolioDelete to portfolio card

--- frontend/src/components/Portfolio/ListPortfolio/ListPortfolio.tsx
+++ frontend/src/components/Portfolio/ListPortfolio/ListPortfolio.tsx
@@ -1,17 +1,18 @@
-import React, { FC } from 'react';
+import React, { FC, SyntheticEvent } from 'react';
 import './ListPortfolio.css';
 import CardPortfolio from '../CardPortfolio/CardPortfolio';
 
 interface ListPortfolioProps {
   portfolio: string[];
+  onPortfolioDelete: (e: SyntheticEvent) => void;
 }
 
-const ListPortfolio: FC<ListPortfolioProps> = ({portfolio}:ListPortfolioProps) => (
+const ListPortfolio: FC<ListPortfolioProps> = ({portfolio,onPortfolioDelete}:ListPortfolioProps) => (
   <div>
     <h3>My Portfolio</h3>
     <ul>
       {portfolio && portfolio.map((symbol, index) => (
-        <CardPortfolio portfolio={portfolio}/>
+        <CardPortfolio portfolio={portfolio} onPortfolioDelete={onPortfolioDelete} />
       ))}
     </ul>
   </div>
