8a40c4c220f1c47b7083a5c63bf2995a7af74057
kifle23 <kiflekifle23@gmail.com>
2024-05-23 17:14:08 +0300

    Update Search

--- frontend/src/components/Search/Search.tsx
+++ frontend/src/components/Search/Search.tsx
@@ -9,17 +9,22 @@ interface Props {
 
 const Search: FC<Props> = ({onSearchSubmit,handleSearchChange,search}:Props) => {
   return (
-  <div className="Search">
-    <form onSubmit={onSearchSubmit} >
-    <input
-      type="text"
-      placeholder="Search..."
-      value={search}
-      onChange={(e)=> handleSearchChange(e)}
-    />
-    </form>
-    
-  </div>
+    <section className="relative bg-gray-100">
+    <div className="max-w-4xl mx-auto p-6 space-y-6">
+      <form
+        className="form relative flex flex-col w-full p-10 space-y-4 bg-darkBlue rounded-lg md:flex-row md:space-y-0 md:space-x-3"
+        onSubmit={onSearchSubmit}
+      >
+        <input
+          className="flex-1 p-3 border-2 rounded-lg placeholder-black focus:outline-none"
+          id="search-input"
+          placeholder="Search companies"
+          value={search}
+          onChange={handleSearchChange}
+        ></input>
+      </form>
+    </div>
+  </section>
   );
 
   };
