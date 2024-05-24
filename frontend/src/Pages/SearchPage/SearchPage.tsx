961e9773a871a7e156ee06aa3eea6549895a23cf
kifle23 <kiflekifle23@gmail.com>
2024-05-24 10:51:35 +0300

    Refactor search

--- frontend/src/Pages/SearchPage/SearchPage.tsx
+++ frontend/src/Pages/SearchPage/SearchPage.tsx
@@ -1,12 +1,64 @@
-import React, { FC } from 'react';
-import styles from './SearchPage.module.css';
+import React, { ChangeEvent, FC, FormEvent, useEffect, useState } from 'react';
+import './SearchPage.css';
+import Hero from '../../components/Hero/Hero';
+import Search from '../../components/Search/Search';
+import ListPortfolio from '../../components/Portfolio/ListPortfolio/ListPortfolio';
+import CardList from '../../components/CardList/CardList';
+import { CompanySearch } from '../../company';
+import { searchCompanies } from '../../api';
 
 interface SearchPageProps {}
 
-const SearchPage: FC<SearchPageProps> = () => (
-  <div className={styles.SearchPage} data-testid="SearchPage">
-    SearchPage Component
+const SearchPage: FC<SearchPageProps> = () => {
+  const [search, setSearch] = useState('');
+  const [portfolio, setPortfolio] = useState<string[]>([]);
+  const [searchResults, setSearchResults] = useState<CompanySearch[]>([]);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    console.log('Updated searchResults:', searchResults);
+  }, [searchResults]);
+
+  const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  const onSearchSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const result = await searchCompanies(search);
+      if (typeof result === 'string') {
+        setError(result);
+      } else if (result && Array.isArray(result.data)) {
+        setSearchResults(result.data);
+      }
+    } catch (error) {
+      setError('An error occurred while fetching data.');
+    }
+  };
+
+  const onPotfolioCreate=(e: any) =>{
+    e.preventDefault();
+    const exists = portfolio.find((value) => value === e.target[0].value);
+    if (exists) return;
+    const updatedPortfolio = [...portfolio, e.target[0].value];
+    setPortfolio(updatedPortfolio);
+  }
+
+  const onPortfolioDelete = (e: any) => {
+    e.preventDefault();
+    const updatedPortfolio = portfolio.filter((value) => value !== e.target[0].value);
+    setPortfolio(updatedPortfolio);
+  }
+  return (
+  <div data-testid="SearchPage">
+      <Hero />
+      <Search onSearchSubmit={onSearchSubmit} handleSearchChange={handleSearchChange} search={search} />
+      <ListPortfolio portfolio={portfolio} onPortfolioDelete={onPortfolioDelete} />
+      <CardList searchResults = {searchResults} onPotfolioCreate = {onPotfolioCreate} />
+      {error && <div>{error}</div>}
   </div>
-);
+  );
+};
 
 export default SearchPage;
