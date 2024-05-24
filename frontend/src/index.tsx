c09f517d54f9455fc891762e41ff49cb271537e5
kifle23 <kiflekifle23@gmail.com>
2024-05-24 10:49:25 +0300

    Update index

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import { searchCompanies } from './api';
 import { RouterProvider } from 'react-router-dom';
 import { routes } from './Routes/Routes';
 
@@ -10,7 +8,6 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-console.log(searchCompanies('tsla'));
 root.render(
   <React.StrictMode>
     <RouterProvider router={routes} />
