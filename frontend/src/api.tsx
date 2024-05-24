83127165996db7ce5ee623108d4a7885728c2bea
kifle23 <kiflekifle23@gmail.com>
2024-05-24 12:52:32 +0300

    Create apicall to get company profile

--- frontend/src/api.tsx
+++ frontend/src/api.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { CompanySearch } from "./company";
+import { CompanyProfile, CompanySearch } from "./company";
 
 interface SearchResponse {
     data: CompanySearch[];
@@ -21,3 +21,18 @@ export const searchCompanies = async (query: string) => {
     }
   }
 }; 
+
+export const getCompanyProfile = async (ticker: string) => {
+  try {
+    const data = await axios.get<CompanyProfile[]>(
+        `https://financialmodelingprep.com/api/v3/profile/${ticker}?apikey=${process.env.REACT_APP_API_KEY}`
+        );
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.log("error message: ", error.message);
+    } else {
+      console.log("unexpected error: ", error);
+    }
+  }
+};
