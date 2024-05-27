9a9d28d8942bf0f7b4de81daa80cd6551b20b635
kifle23 <kiflekifle23@gmail.com>
2024-05-27 20:29:23 +0300

    Create Income Statment

---
+++ frontend/src/components/IncomeStatement/IncomeStatement.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import './IncomeStatement.css';
+
+interface IncomeStatementProps {}
+
+const IncomeStatement: FC<IncomeStatementProps> = () => (
+  <div className="IncomeStatement">
+    IncomeStatement Component
+  </div>
+);
+
+export default IncomeStatement;
