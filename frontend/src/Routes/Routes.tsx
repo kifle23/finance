5b32cbceb3212a530b440755628563e7c7b2891b
kifle23 <kiflekifle23@gmail.com>
2024-05-27 20:28:09 +0300

    Add nested routes for company profile and income statment

--- frontend/src/Routes/Routes.tsx
+++ frontend/src/Routes/Routes.tsx
@@ -3,26 +3,30 @@ import App from "../App";
 import HomePage from "../Pages/HomePage/HomePage";
 import SearchPage from "../Pages/SearchPage/SearchPage";
 import CompanyPage from "../Pages/CompanyPage/CompanyPage";
+import CompanyProfile from "../components/CompanyProfile/CompanyProfile";
+import IncomeStatement from "../components/IncomeStatement/IncomeStatement";
 
-export const routes = createBrowserRouter( [
-    {
+export const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
         path: "/",
-        element: <App />,
+        element: <HomePage />,
+      },
+      {
+        path: "search",
+        element: <SearchPage />,
+      },
+      {
+        path: "company/:ticker",
+        element: <CompanyPage />,
         children: [
-            {
-                path: "/", 
-                element: <HomePage />
-                
-            },
-            {
-                path: "search", 
-                element: <SearchPage />
-                
-            },
-            {
-                path: "company/:ticker", 
-                element: <CompanyPage />
-                
-            },
-        ]
-    }]);
+          { path: "company-profile", element: <CompanyProfile /> },
+          { path: "income-statement", element: <IncomeStatement /> },
+        ],
+      },
+    ],
+  },
+]);
