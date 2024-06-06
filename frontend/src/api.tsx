0bd20d67af951ab878b42c06ee59729fa1e1ae0f
kifle23 <kiflekifle23@gmail.com>
2024-06-06 21:01:27 +0300

    Create api end point for getKeyMetrics

--- frontend/src/api.tsx
+++ frontend/src/api.tsx
@@ -1,15 +1,15 @@
 import axios from "axios";
-import { CompanyProfile, CompanySearch } from "./company";
+import { CompanyKeyMetrics, CompanyProfile, CompanySearch } from "./company";
 
 interface SearchResponse {
-    data: CompanySearch[];
+  data: CompanySearch[];
 }
 
 export const searchCompanies = async (query: string) => {
   try {
     const data = await axios.get<SearchResponse>(
-        `https://financialmodelingprep.com/api/v3/search-ticker?query=${query}&limit=10&exchange=NASDAQ&apikey=${process.env.REACT_APP_API_KEY}`
-        );
+      `https://financialmodelingprep.com/api/v3/search-ticker?query=${query}&limit=10&exchange=NASDAQ&apikey=${process.env.REACT_APP_API_KEY}`
+    );
     return data;
   } catch (error) {
     if (axios.isAxiosError(error)) {
@@ -20,13 +20,28 @@ export const searchCompanies = async (query: string) => {
       return "An expected error has occured.";
     }
   }
-}; 
+};
 
 export const getCompanyProfile = async (ticker: string) => {
   try {
     const data = await axios.get<CompanyProfile[]>(
-        `https://financialmodelingprep.com/api/v3/profile/${ticker}?apikey=${process.env.REACT_APP_API_KEY}`
-        );
+      `https://financialmodelingprep.com/api/v3/profile/${ticker}?apikey=${process.env.REACT_APP_API_KEY}`
+    );
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.log("error message: ", error.message);
+    } else {
+      console.log("unexpected error: ", error);
+    }
+  }
+};
+
+export const getKeyMetrics = async (ticker: string) => {
+  try {
+    const data = await axios.get<CompanyKeyMetrics[]>(
+      `https://financialmodelingprep.com/api/v3/key-metrics-ttm/${ticker}?limit=40&apikey=${process.env.REACT_APP_API_KEY}`
+    );
     return data;
   } catch (error) {
     if (axios.isAxiosError(error)) {
