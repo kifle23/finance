a58c056ef2942453659104cd35190e4740f051f6
kifle23 <kiflekifle23@gmail.com>
2024-06-06 21:09:00 +0300

    Create TTM Profile

--- frontend/src/components/CompanyProfile/CompanyProfile.tsx
+++ frontend/src/components/CompanyProfile/CompanyProfile.tsx
@@ -1,12 +1,116 @@
-import React, { FC } from 'react';
-import './CompanyProfile.css';
+import React, { FC, useEffect, useState } from "react";
+import "./CompanyProfile.css";
+import { CompanyKeyMetrics } from "../../company";
+import { useOutletContext } from "react-router-dom";
+import { getKeyMetrics } from "../../api";
 
 interface CompanyProfileProps {}
 
-const CompanyProfile: FC<CompanyProfileProps> = () => (
-  <div className="CompanyProfile">
-    CompanyProfile Component
-  </div>
-);
+const tableConfig = [
+  {
+    label: "Market Cap",
+    render: (company: CompanyKeyMetrics) => company.marketCapTTM,
+    subTitle: "Total value of all a company's shares of stock",
+  },
+  {
+    label: "Current Ratio",
+    render: (company: CompanyKeyMetrics) => company.currentRatioTTM,
+    subTitle:
+      "Measures the companies ability to pay short term debt obligations",
+  },
+  {
+    label: "Return On Equity",
+    render: (company: CompanyKeyMetrics) => company.roeTTM,
+    subTitle:
+      "Return on equity is the measure of a company's net income divided by its shareholder's equity",
+  },
+  {
+    label: "Return On Assets",
+    render: (company: CompanyKeyMetrics) => company.returnOnTangibleAssetsTTM,
+    subTitle:
+      "Return on assets is the measure of how effective a company is using its assets",
+  },
+  {
+    label: "Free Cashflow Per Share",
+    render: (company: CompanyKeyMetrics) => company.freeCashFlowPerShareTTM,
+    subTitle:
+      "Return on assets is the measure of how effective a company is using its assets",
+  },
+  {
+    label: "Book Value Per Share TTM",
+    render: (company: CompanyKeyMetrics) => company.bookValuePerShareTTM,
+    subTitle:
+      "Book value per share indicates a firm's net asset value (total assets - total liabilities) on per share basis",
+  },
+  {
+    label: "Divdend Yield TTM",
+    render: (company: CompanyKeyMetrics) => company.dividendYieldTTM,
+    subTitle: "Shows how much a company pays each year relative to stock price",
+  },
+  {
+    label: "Capex Per Share TTM",
+    render: (company: CompanyKeyMetrics) => company.capexPerShareTTM,
+    subTitle:
+      "Capex is used by a company to aquire, upgrade, and maintain physical assets",
+  },
+  {
+    label: "Graham Number",
+    render: (company: CompanyKeyMetrics) => company.grahamNumberTTM,
+    subTitle:
+      "This is the upperbouind of the price range that a defensive investor should pay for a stock",
+  },
+  {
+    label: "PE Ratio",
+    render: (company: CompanyKeyMetrics) => company.peRatioTTM,
+    subTitle:
+      "This is the upperbouind of the price range that a defensive investor should pay for a stock",
+  },
+];
+
+const CompanyProfile: FC<CompanyProfileProps> = () => {
+  const ticker = useOutletContext<string>();
+  const [company, setCompany] = useState<CompanyKeyMetrics>();
+  useEffect(() => {
+    const fetchCompany = async () => {
+      const result = await getKeyMetrics(ticker!);
+      setCompany(result?.data[0]);
+    };
+    fetchCompany();
+  }, [ticker]);
+  
+  return (
+    <div className="CompanyProfile">
+      {company ? (
+        <div className="w-full relative flex ct-docs-disable-sidebar-content overflow-x-hidden">
+          <div className="bg-white shadow rounded-lg ml-4 mt-4 mb-4 p-4 sm:p-6 w-full">
+            <ul className="divide-y divide-gray-200">
+              {tableConfig.map((row) => (
+                <li className="py-6 sm:py-6">
+                  <div className="flex items-center space-x-4">
+                    <div className="flex-1 min-w-0">
+                      <p className="text-sm font-medium text-gray-900 truncate">
+                        {row.label}
+                      </p>
+                      <p className="text-sm text-gray-500 truncate">
+                        {row.subTitle && row.subTitle}
+                      </p>
+                    </div>
+                    <div className="inline-flex items-center text-base font-semibold text-gray-900">
+                      {row.render(company)}
+                    </div>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+};
+
 
 export default CompanyProfile;
+
