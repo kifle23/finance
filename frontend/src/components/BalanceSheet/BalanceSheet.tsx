bb2d1dd28c8753727dac9a1f36a5559143fbeb30
kifle23 <kiflekifle23@gmail.com>
2024-06-07 18:41:02 +0300

    Generate BS Component

---
+++ frontend/src/components/BalanceSheet/BalanceSheet.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import './BalanceSheet.css';
+
+interface BalanceSheetProps {}
+
+const BalanceSheet: FC<BalanceSheetProps> = () => (
+  <div className="BalanceSheet">
+    BalanceSheet Component
+  </div>
+);
+
+export default BalanceSheet;
