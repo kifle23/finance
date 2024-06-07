e9c7adc6e79a14b063e4563374ea8a76f5f5ac4b
kifle23 <kiflekifle23@gmail.com>
2024-06-07 18:43:35 +0300

    Add config to BS

--- frontend/src/components/Sidebar/Sidebar.tsx
+++ frontend/src/components/Sidebar/Sidebar.tsx
@@ -19,15 +19,22 @@ const Sidebar: FC<SidebarProps> = () => (
             className="flex md:min-w-full text-blueGray-500 text-xs uppercase font-bold block pt-1 pb-4 no-underline"
           >
             <FaHome />
-              <h6 className="ml-3">Company Profile</h6>
+            <h6 className="ml-3">Company Profile</h6>
           </Link>
           <Link
-              to="income-statement"
-              className="flex md:min-w-full text-blueGray-500 text-xs uppercase font-bold block pt-1 pb-4 no-underline"
-            >
-              <FaTable />
-              <h6 className="ml-3">Income Statement</h6>
-            </Link>
+            to="income-statement"
+            className="flex md:min-w-full text-blueGray-500 text-xs uppercase font-bold block pt-1 pb-4 no-underline"
+          >
+            <FaTable />
+            <h6 className="ml-3">Income Statement</h6>
+          </Link>
+          <Link
+            to="balance-sheet"
+            className="flex md:min-w-full text-blueGray-500 text-xs uppercase font-bold block pt-1 pb-4 no-underline"
+          >
+            <FaTable />
+            <h6 className="ml-3">Balance Sheet</h6>
+          </Link>
         </div>
       </div>
     </div>
