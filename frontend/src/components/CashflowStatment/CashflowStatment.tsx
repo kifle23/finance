f56800fa186e2f48856808e555bddb5fcf2b7d72
kifle23 <kiflekifle23@gmail.com>
2024-06-08 13:36:43 +0300

    Create cashflow statment

---
+++ frontend/src/components/CashflowStatment/CashflowStatment.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import './CashflowStatment.css';
+
+interface CashflowStatmentProps {}
+
+const CashflowStatment: FC<CashflowStatmentProps> = () => (
+  <div className="CashflowStatment">
+    CashflowStatment Component
+  </div>
+);
+
+export default CashflowStatment;
