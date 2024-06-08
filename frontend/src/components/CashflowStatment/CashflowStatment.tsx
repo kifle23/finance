8d268b53a5b876bae210f157a2478795300a2970
kifle23 <kiflekifle23@gmail.com>
2024-06-08 13:39:35 +0300

    Add config to CFS

--- frontend/src/components/CashflowStatment/CashflowStatment.tsx
+++ frontend/src/components/CashflowStatment/CashflowStatment.tsx
@@ -1,12 +1,50 @@
 import React, { FC } from 'react';
 import './CashflowStatment.css';
+import { CompanyCashFlow } from '../../company';
 
 interface CashflowStatmentProps {}
 
+const config = [
+  {
+    label: "Date",
+    render: (company: CompanyCashFlow) => company.date,
+  },
+  {
+    label: "Operating Cashflow",
+    render: (company: CompanyCashFlow) => company.operatingCashFlow,
+  },
+  {
+    label: "Investing Cashflow",
+    render: (company: CompanyCashFlow) =>
+      company.netCashUsedForInvestingActivites,
+  },
+  {
+    label: "Financing Cashflow",
+    render: (company: CompanyCashFlow) =>
+      company.netCashUsedProvidedByFinancingActivities,
+  },
+  {
+    label: "Cash At End of Period",
+    render: (company: CompanyCashFlow) => company.cashAtEndOfPeriod,
+  },
+  {
+    label: "CapEX",
+    render: (company: CompanyCashFlow) => company.capitalExpenditure,
+  },
+  {
+    label: "Issuance Of Stock",
+    render: (company: CompanyCashFlow) => company.commonStockIssued,
+  },
+  {
+    label: "Free Cash Flow",
+    render: (company: CompanyCashFlow) => company.freeCashFlow,
+  },
+];
+
 const CashflowStatment: FC<CashflowStatmentProps> = () => (
-  <div className="CashflowStatment">
+  <>
     CashflowStatment Component
-  </div>
+  </>
 );
 
 export default CashflowStatment;
