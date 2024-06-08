ea54ce1d5a00183bf5dfd78a19c2f50676322ac3
kifle23 <kiflekifle23@gmail.com>
2024-06-08 14:19:36 +0300

    Setup spinner

---
+++ frontend/src/components/Spinner/Spinner.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import './Spinner.css';
+
+interface SpinnerProps {}
+
+const Spinner: FC<SpinnerProps> = () => (
+  <div className="Spinner">
+    Spinner Component
+  </div>
+);
+
+export default Spinner;
