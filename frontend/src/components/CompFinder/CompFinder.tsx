6a0c5d2da1d4266d6c7d0612ea0c2ade336d4fa3
kifle23 <kiflekifle23@gmail.com>
2024-06-11 14:14:24 +0300

    Generate CompFinder component

---
+++ frontend/src/components/CompFinder/CompFinder.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import './CompFinder.css';
+
+interface CompFinderProps {}
+
+const CompFinder: FC<CompFinderProps> = () => (
+  <div className="CompFinder">
+    CompFinder Component
+  </div>
+);
+
+export default CompFinder;
