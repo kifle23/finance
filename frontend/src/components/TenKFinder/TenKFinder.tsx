4ac554e840f1798ada637d6137d28dc62331d2e7
kifle23 <kiflekifle23@gmail.com>
2024-06-12 10:20:06 +0300

    Generate ten-k finder component

---
+++ frontend/src/components/TenKFinder/TenKFinder.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import './TenKFinder.css';
+
+interface TenKFinderProps {}
+
+const TenKFinder: FC<TenKFinderProps> = () => (
+  <div className="TenKFinder">
+    TenKFinder Component
+  </div>
+);
+
+export default TenKFinder;
