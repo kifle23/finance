9bbfd1c67554238a9a9ca6c1e271df02c018f950
kifle23 <kiflekifle23@gmail.com>
2024-06-12 10:51:47 +0300

    Implment tenk item finder

--- frontend/src/components/TenKFinderItem/TenKFinderItem.tsx
+++ frontend/src/components/TenKFinderItem/TenKFinderItem.tsx
@@ -1,12 +1,28 @@
-import React, { FC } from 'react';
-import './TenKFinderItem.css';
+import React, { FC } from "react";
+import "./TenKFinderItem.css";
+import { CompanyTenK } from "../../company";
+import { Link } from "react-router-dom";
 
-interface TenKFinderItemProps {}
+interface TenKFinderItemProps {
+  tenK: CompanyTenK;
+}
 
-const TenKFinderItem: FC<TenKFinderItemProps> = () => (
-  <div className="TenKFinderItem">
-    TenKFinderItem Component
-  </div>
-);
+const TenKFinderItem: FC<TenKFinderItemProps> = ({
+  tenK,
+}: TenKFinderItemProps) => {
+  const fillingDate = new Date(tenK.fillingDate).getFullYear();
+
+  return (
+    <Link
+      reloadDocument
+      to={tenK.finalLink}
+      type="button"
+      className="inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-lightGreen border border-gray-200 rounded-l-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
+    >
+      10K - {tenK.symbol} - {fillingDate}
+    </Link>
+  );
+};
 
 export default TenKFinderItem;
+
