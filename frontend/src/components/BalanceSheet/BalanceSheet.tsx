be6cf4b0a9e73eaf769ce427fa07410336308543
kifle23 <kiflekifle23@gmail.com>
2024-06-12 16:01:55 +0300

    Add price formatting in BS

--- frontend/src/components/BalanceSheet/BalanceSheet.tsx
+++ frontend/src/components/BalanceSheet/BalanceSheet.tsx
@@ -5,66 +5,85 @@ import { useOutletContext } from "react-router-dom";
 import { getBalanceSheet } from "../../api";
 import RatioList from "../RatioList/RatioList";
 import Spinner from "../Spinner/Spinner";
+import { formatLargeMonetaryNumber } from "../../Helpers/NumberFormatting";
 
 interface BalanceSheetProps {}
+
+interface OutletContext {
+  ticker: string;
+  description: string;
+}
 
 const config = [
   {
     label: <div className="font-bold">Total Assets</div>,
-    render: (company: CompanyBalanceSheet) => company.totalAssets,
+    render: (company: CompanyBalanceSheet) =>
+      formatLargeMonetaryNumber(company.totalAssets),
   },
   {
     label: "Current Assets",
-    render: (company: CompanyBalanceSheet) => company.totalCurrentAssets,
+    render: (company: CompanyBalanceSheet) =>
+      formatLargeMonetaryNumber(company.totalCurrentAssets),
   },
   {
     label: "Total Cash",
-    render: (company: CompanyBalanceSheet) => company.cashAndCashEquivalents,
+    render: (company: CompanyBalanceSheet) =>
+      formatLargeMonetaryNumber(company.cashAndCashEquivalents),
   },
   {
-    label: "Property & Equipment",
-    render: (company: CompanyBalanceSheet) => company.propertyPlantEquipmentNet,
+    label: "Property & equipment",
+    render: (company: CompanyBalanceSheet) =>
+      formatLargeMonetaryNumber(company.propertyPlantEquipmentNet),
   },
   {
     label: "Intangible Assets",
-    render: (company: CompanyBalanceSheet) => company.intangibleAssets,
+    render: (company: CompanyBalanceSheet) =>
+      formatLargeMonetaryNumber(company.intangibleAssets),
   },
   {
     label: "Long Term Debt",
-    render: (company: CompanyBalanceSheet) => company.longTermDebt,
+    render: (company: CompanyBalanceSheet) =>
+      formatLargeMonetaryNumber(company.longTermDebt),
   },
   {
     label: "Total Debt",
-    render: (company: CompanyBalanceSheet) => company.otherCurrentLiabilities,
+    render: (company: CompanyBalanceSheet) =>
+      formatLargeMonetaryNumber(company.otherCurrentLiabilities),
   },
   {
-    label: <div className="font-bold">Total Liabilities</div>,
-    render: (company: CompanyBalanceSheet) => company.totalLiabilities,
+    label: <div className="font-bold">Total Liabilites</div>,
+    render: (company: CompanyBalanceSheet) =>
+      formatLargeMonetaryNumber(company.totalLiabilities),
   },
   {
     label: "Current Liabilities",
-    render: (company: CompanyBalanceSheet) => company.totalCurrentLiabilities,
+    render: (company: CompanyBalanceSheet) =>
+      formatLargeMonetaryNumber(company.totalCurrentLiabilities),
   },
   {
     label: "Long-Term Debt",
-    render: (company: CompanyBalanceSheet) => company.longTermDebt,
+    render: (company: CompanyBalanceSheet) =>
+      formatLargeMonetaryNumber(company.longTermDebt),
   },
   {
     label: "Long-Term Income Taxes",
-    render: (company: CompanyBalanceSheet) => company.otherLiabilities,
+    render: (company: CompanyBalanceSheet) =>
+      formatLargeMonetaryNumber(company.otherLiabilities),
   },
   {
-    label: "Stockholder's Equity",
-    render: (company: CompanyBalanceSheet) => company.totalStockholdersEquity,
+    label: "Stakeholder's Equity",
+    render: (company: CompanyBalanceSheet) =>
+      formatLargeMonetaryNumber(company.totalStockholdersEquity),
   },
   {
     label: "Retained Earnings",
-    render: (company: CompanyBalanceSheet) => company.retainedEarnings,
+    render: (company: CompanyBalanceSheet) =>
+      formatLargeMonetaryNumber(company.retainedEarnings),
   },
 ];
 
 const BalanceSheet: FC<BalanceSheetProps> = () => {
-  const ticker = useOutletContext<string>();
+  const { ticker } = useOutletContext<OutletContext>();
   const [balanceSheet, setBalanceSheet] = useState<CompanyBalanceSheet>();
   const [error, setError] = useState<string>();
 
@@ -81,15 +100,11 @@ const BalanceSheet: FC<BalanceSheetProps> = () => {
   }, [ticker]);
 
   const props = { data: balanceSheet, config };
-  
+
   return (
     <>
       {error && <div>{error}</div>}
-      {balanceSheet ? (
-        <RatioList {...props} />
-      ) : (
-        <Spinner />
-      )}
+      {balanceSheet ? <RatioList {...props} /> : <Spinner />}
     </>
   );
 };
