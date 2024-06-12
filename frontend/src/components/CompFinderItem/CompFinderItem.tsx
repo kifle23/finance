0255997d4f113731d0577b64c3cacd07adf38da6
kifle23 <kiflekifle23@gmail.com>
2024-06-12 08:49:00 +0300

    Generate  comp finder item component

---
+++ frontend/src/components/CompFinderItem/CompFinderItem.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import './CompFinderItem.css';
+
+interface CompFinderItemProps {}
+
+const CompFinderItem: FC<CompFinderItemProps> = () => (
+  <div className="CompFinderItem">
+    CompFinderItem Component
+  </div>
+);
+
+export default CompFinderItem;
