c1489fc2bb17d035525c2e83563b90716eb7ae96
kifle23 <kiflekifle23@gmail.com>
2024-06-13 12:17:39 +0300

    Implment interface for stock repo

---
+++ api/Repository/StockRepository.cs
@@ -0,0 +1,62 @@
+using api.Data;
+using api.Dtos.Stock;
+using api.Interfaces;
+using api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Repository
+{
+    public class StockRepository(ApplicationDBContext context) : IStockRepository
+    {
+        private readonly ApplicationDBContext _context = context;
+
+        public async Task<Stock> CreateAsync(Stock stock)
+        {
+            await _context.Stocks.AddAsync(stock);
+            await _context.SaveChangesAsync();
+            return stock;
+        }
+
+        public async Task<Stock?> DeleteAsync(int id)
+        {
+            var stock = await _context.Stocks.FirstOrDefaultAsync(x => x.Id == id);
+            if (stock == null)
+            {
+                return null;
+            }
+
+            _context.Stocks.Remove(stock);
+            await _context.SaveChangesAsync();
+            return stock;
+        }
+
+        public async Task<Stock?> GetByIdAsync(int id)
+        {
+            return await _context.Stocks.FindAsync(id);
+        }
+
+        public async Task<List<Stock>> GetAllAsync()
+        {
+            return await _context.Stocks.ToListAsync();
+        }
+
+        public async Task<Stock?> UpdateAsync(int id, StockPutDto stock)
+        {
+            var existingStock = await _context.Stocks.FirstOrDefaultAsync(x => x.Id == id);
+            if (existingStock == null)
+            {
+                return null;
+            }
+
+            existingStock.Symbol = stock.Symbol;
+            existingStock.Name = stock.Name;
+            existingStock.Purchase = stock.Purchase;
+            existingStock.LastDiv = stock.LastDiv;
+            existingStock.Industry = stock.Industry;
+            existingStock.MarketCap = stock.MarketCap;
+
+            await _context.SaveChangesAsync();
+            return existingStock;
+        }
+    }
+}
