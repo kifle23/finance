92f50d55be5c4c7060679c4ea116332af9f27659
kifle23 <kiflekifle23@gmail.com>
2024-06-13 10:33:10 +0300

    Create dto for putting stocks

---
+++ api/Dtos/Stock/StockPutDto.cs
@@ -0,0 +1,14 @@
+namespace api.Dtos.Stock
+{
+    public class StockPutDto
+    {
+        public string Symbol { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+
+        public decimal Purchase { get; set; }
+
+        public decimal LastDiv { get; set; }
+        public string Industry { get; set; } = string.Empty;
+        public long MarketCap { get; set; }
+    }
+}
