dce9288177873e34c9f155b3565600862ec30250
kifle23 <kiflekifle23@gmail.com>
2024-06-13 19:53:49 +0300

    Create comment dtos

---
+++ api/Dtos/Comment/CreateCommentDto.cs
@@ -0,0 +1,8 @@
+namespace api.Dtos.Comment
+{
+    public class CreateCommentDto
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+    }
+}
