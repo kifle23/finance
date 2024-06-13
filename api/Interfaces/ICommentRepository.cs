3c867a63057d2b2a566cd257ef82f249fbf9932a
kifle23 <kiflekifle23@gmail.com>
2024-06-13 19:49:10 +0300

    Create interface for comment repository

---
+++ api/Interfaces/ICommentRepository.cs
@@ -0,0 +1,13 @@
+using api.Models;
+
+namespace api.Interfaces
+{
+    public interface ICommentRepository
+    {
+        Task<List<Comment>> GetAllAsync();
+        Task<Comment?> GetByIdAsync(int id);
+        Task<Comment> CreateAsync(Comment comment);
+        Task<Comment?> UpdateAsync(int id, Comment comment);
+        Task<Comment?> DeleteAsync(int id);
+    }
+}
